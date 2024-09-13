58a30de4684abf2d0d0e8b7e6346af7400c654f3
Raghav0789 <sharma33432@gmail.com>
2024-09-13 11:01:19 +0530

    uploading code

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express();
+const user = require('./routes/user');
+
+app.use(user)
+
+
+
+
+
+app.listen(3000,(err)=>{
+    if(err){
+        console.log(err);
+        
+    }
+    else{
+        console.log("Server is listening on port 3000");
+        
+    }
+})
---
+++ routes/user.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const userController = require('../controllers/UserController')
+const router = express.Router();
+
+router.get('/users',(req,res)=>{
+    userController.getAllUsers(req,res);
+})
+
+module.exports=router
